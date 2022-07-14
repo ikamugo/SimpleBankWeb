19d2f2cad974e2039e52dd095e3dd2d6f8b6abd9
Isaac Kamugo <ikamugo@gmail.com>
2022-07-14 22:19:42 +0300

    - Corrected daily transaction limit to 800,000 - Renamed method "MonthlyAccountProcess" to "PerformMonthEndProcessing"

--- SimpleBankWeb/Business/Models/CurrentAccount.cs
+++ SimpleBankWeb/Business/Models/CurrentAccount.cs
@@ -6,7 +6,7 @@ public class CurrentAccount : Account
 {
     private const double _minimumBalance = 20000;
     private const double _monthlyCharge = 3000;
-    private const double _dailyTransactionLimit = 80000;
+    private const double _dailyTransactionLimit = 800000;
 
     public CurrentAccount(string number, string name, double initialDeposit) 
         : base(number, name, initialDeposit)
@@ -15,7 +15,7 @@ public class CurrentAccount : Account
             throw new ArgumentOutOfRangeException(nameof(initialDeposit));
     }
 
-    public override void MonthlyAccountProcess()
+    public override void PerformMonthEndProcessing()
     {
         var monthlyCharge = Transaction.Debit(this.Number, _monthlyCharge, "Monthly account fees");
         Transactions.Add(monthlyCharge);
