83e89945bdff8b9cd280ae8b694ca3a7863a48b6
Isaac Kamugo <ikamugo@gmail.com>
2022-07-14 22:16:48 +0300

    - Fleshed out the detailed implementation of CreditAccount - Added CreditType enum to differentiate the different credit categories available (silver, gold, platinum)

--- SimpleBankWeb/Business/Models/CreditAccount.cs
+++ SimpleBankWeb/Business/Models/CreditAccount.cs
@@ -1,21 +1,81 @@
-﻿namespace SimpleBankWeb.Business.Models
+﻿using SimpleBankWeb.Business.EnumTypes;
+
+namespace SimpleBankWeb.Business.Models
 {
     public class CreditAccount : Account
     {
-        public CreditAccount(string number, string name, double minimumBalance) 
-            : base(number, name, minimumBalance)
-        {
+        private const double _interestRate = 0.05;
+        private const double _overdraftFees = 10000;
 
+        public CreditType Type { get; private set; }
+
+        public double CreditLimit
+        {
+            get
+            {
+                if (Type == CreditType.Platinum)
+                    return 10000000;
+                else if (Type == CreditType.Gold)
+                    return 5000000;
+                else
+                    return 1000000;
+            }
         }
 
-        public override void MonthlyAccountProcess()
+        private CreditAccount(CreditType type, string number, string name, double initialDeposit)
+            : base(number, name, initialDeposit)
         {
-            throw new NotImplementedException();
+            Type = type;
+
+            if(initialDeposit < CreditLimit)
+                throw new ArgumentOutOfRangeException(nameof(initialDeposit));
         }
+
+
+
+        public override void PerformMonthEndProcessing()
+        {
+            var balance = GetBalance();
+            if (balance < 0)
+            {
+                var interestFee = Math.Abs(balance) * _interestRate;
+                Transactions.Add(Transaction.Debit(Number, interestFee, "Interest fees on credit"));
+            }
+        }
+
 
         public override void Withdraw(double amount, string description)
         {
-            throw new NotImplementedException();
+            //check if transaction will result in negative balance, apply overdraft fees
+            var withdrawFees = GetBalance() - amount < 0 ? _overdraftFees : 0;
+
+            if (GetBalance() + CreditLimit < amount + withdrawFees)
+                throw new InvalidOperationException("Insufficient funds to complete the transaction");
+
+            //add the withdraw
+            Transactions.Add(Transaction.Withdraw(Number, amount, description));
+
+            //add withdraw fees if applicable
+            if(withdrawFees > 0)
+            {
+                Transactions.Add(Transaction.Debit(Number, _overdraftFees, "Account overdraft fees"));
+            }
+
+        }
+
+        public static CreditAccount Silver(string number, string name, double minimumBalance)
+        {
+            return new CreditAccount(CreditType.Silver, number, name, minimumBalance);
+        }
+
+        public static CreditAccount Gold(string number, string name, double minimumBalance)
+        {
+            return new CreditAccount(CreditType.Gold, number, name, minimumBalance);
+        }
+
+        public static CreditAccount Platinum(string number, string name, double minimumBalance)
+        {
+            return new CreditAccount(CreditType.Platinum, number, name, minimumBalance);
         }
     }
 }
